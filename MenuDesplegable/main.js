a8f84e5b9c78498bb0c2c7621556b3e9e98ae9bf
Lennin Valle <lenninvallea@gmail.com>
2022-10-31 09:17:50 -0500

    cambios

--- MenuDesplegable/main.js
+++ MenuDesplegable/main.js
@@ -1,30 +1,94 @@
-const btnDepartamentos = document.getElementById('btn-departamentos'), 
-	  grid = document.getElementById('grid');
+const btnDepartamentos = document.getElementById('btn-departamentos'),
+	  btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
+	  grid = document.getElementById('grid'),
+	  contenedorEnlacesNav = document.querySelector('#menu .contenedor-enlace-nav'),
+	  contenedorSubCategorias = document.querySelector('#grid .contenedor-subcategorias'),
 	  esDispositivoMovil = () => window.innerWidth <= 800;
 
 
 btnDepartamentos.addEventListener('mouseover', () => {
 	if(!esDispositivoMovil()){
-	grid.classList.add('activo');		
+		grid.classList.add('activo');		
 	}
 
 });
 
 grid.addEventListener('mouseleave',()=>{
-	if (!esDispositivoMovil()) {}
-	grid.classList.remove('activo');
+	if (!esDispositivoMovil()) {
+		grid.classList.remove('activo');
+	}
 });
 
 document.querySelectorAll('#menu .categorias a').forEach((elemento) =>{
 	elemento.addEventListener('mouseenter',(e)=>{
-		document.querySelectorAll('#menu .subcategoria').forEach((categoria)=>{
-			categoria.classList.remove('activo');
-			if(categoria.dataset.categoria == e.target.dataset.categoria){
-				categoria.classList.add('activo');
-			}
-		});
+		if(!esDispositivoMovil()){
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria)=>{
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				};
+			});
+		};
 	});
 });
 
+/*EventListener para dispositivo movil*/
+document.querySelector('#btn-menu-barras').addEventListener('click', (e) =>{
+	e.preventDefault();
+	if(contenedorEnlacesNav.classList.contains('activo')){
+		contenedorEnlacesNav.classList.remove('activo');
+		document.querySelector('body').style.overflow ='visible';
+	} else{
+		contenedorEnlacesNav.classList.add('activo');
+		document.querySelector('body').style.overflow = 'hidden';
+	}
+});
+
+/*Clic en boton de todos los departamentos (para version movil)*/
+btnDepartamentos.addEventListener('click', (e) =>{
+	e.preventDefault()
+	grid.classList.add('activo');
+	btnCerrarMenu.classList.add('activo');
+});
+
+/*Boton de regresar en el menu de catergorias*/
+document.querySelector('#grid .categorias .btn-regresar').addEventListener('click', (e) =>{
+	e.preventDefault();
+	grid.classList.remove('activo');
+	btnCerrarMenu.classList.remove('activo');
+})
 
 
+document.querySelectorAll('#menu .categorias a').forEach((elemento)=>{
+	elemento.addEventListener('click', (e) => {
+		if(esDispositivoMovil()){
+			contenedorSubCategorias.classList.add('activo');
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria) =>{
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+
+				}
+
+			})
+		}
+	})
+})
+
+/*Boton de regresar en el menu de catergorias*/
+document.querySelectorAll('#grid .contenedor-subcategorias .btn-regresar').forEach((boton)=>{
+	boton.addEventListener('click', (e) =>{
+		e.preventDefault();
+		contenedorSubCategorias.classList.remove('activo');
+
+	});
+});
+
+btnCerrarMenu.addEventListener('click', (e) =>{
+	e.preventDefault();
+	document.querySelectorAll('#menu .activo').forEach((elemento)=>{
+		elemento.classList.remove('activo');
+	});
+
+	document.querySelector('body').style.overflow='visible';
+});
